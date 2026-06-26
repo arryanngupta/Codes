dcb7fa6bc7707d39c0cdc4be2a47d1dc32053046
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-06-27 00:30:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.79%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
 
-    vector<vector<int>> dp;
+    // vector<vector<int>> dp;
 
-    bool recFind(int idx,int cnt,string &s,int n){
-        if(cnt<0) return false;
-        if(idx==n) return (cnt==0);
-        if(dp[idx][cnt]!=-1) return dp[idx][cnt];
-        bool ans = false;
-        if(s[idx]=='('){
-            ans = recFind(idx+1,cnt+1,s,n);
-        }
-        else if(s[idx]==')'){
-            ans = recFind(idx+1,cnt-1,s,n);
-        }
-        else{
-            ans = ans||recFind(idx+1,cnt+1,s,n);
-            ans = ans||recFind(idx+1,cnt,s,n);
-            ans = ans||recFind(idx+1,cnt-1,s,n);
-        }
-        return dp[idx][cnt]=ans;
-    }
+    // bool recFind(int idx,int cnt,string &s,int n){
+    //     if(cnt<0) return false;
+    //     if(idx==n) return (cnt==0);
+    //     if(dp[idx][cnt]!=-1) return dp[idx][cnt];
+    //     bool ans = false;
+    //     if(s[idx]=='('){
+    //         ans = recFind(idx+1,cnt+1,s,n);
+    //     }
+    //     else if(s[idx]==')'){
+    //         ans = recFind(idx+1,cnt-1,s,n);
+    //     }
+    //     else{
+    //         ans = ans||recFind(idx+1,cnt+1,s,n);
+    //         ans = ans||recFind(idx+1,cnt,s,n);
+    //         ans = ans||recFind(idx+1,cnt-1,s,n);
+    //     }
+    //     return dp[idx][cnt]=ans;
+    // }
 
     bool checkValidString(string s) {
-        int n = s.size();
-        dp.resize(n,vector<int> (n+1,-1));
-        return recFind(0,0,s,n);
+        // int n = s.size();
+        // dp.resize(n,vector<int> (n+1,-1));
+        // return recFind(0,0,s,n);
+        int maxi = 0,mini = 0;
+        for(auto it: s){
+            if(it=='('){
+                mini++;
+                maxi++;
+            }
+            else if(it==')'){
+                mini--;
+                maxi--;
+            }
+            else{
+                mini--;
+                maxi++;
+            }
+            if(maxi<0) return false;
+            mini = max(0,mini);
+        }
+        return mini==0;
     }
 };
