c74a98d6bd6644349208f72acf116760ebd92183
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-07-13 15:07:51 +0530

    Time: 314 ms (36.49%), Space: 225.9 MB (23.86%) - LeetHub

---
+++ 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    vector<vector<int>> adjList,dp;
+
+    int recFind(int node,int par,int cnt,vector<int>& coins,int k,int n){
+        if(adjList[node].size()==0){
+            int val = coins[node]/(1<<cnt);
+            return max(val-k,val/2);
+        }
+        if(dp[node][cnt]!=-1) return dp[node][cnt];
+        int val = coins[node]/(1<<cnt);
+        int op1 = val-k,op2 = val/2;
+        for(auto it: adjList[node]){
+            if(it==par) continue;
+            op1 += recFind(it,node,cnt,coins,k,n);
+            op2 += recFind(it,node,min(19,cnt+1),coins,k,n);
+        }
+        return dp[node][cnt]=max(op1,op2);
+    }
+
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
+        int n = coins.size();
+        adjList.resize(n);
+        for(auto it: edges){
+            adjList[it[0]].push_back(it[1]);
+            adjList[it[1]].push_back(it[0]);
+        }
+        dp.resize(n,vector<int> (20,-1));
+        return recFind(0,-1,0,coins,k,n);
+    }
+};
