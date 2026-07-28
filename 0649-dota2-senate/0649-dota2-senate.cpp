48c9df21c33cfc5897b9961ec5e606dfbcfdc65e
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-07-28 09:01:12 +0530

    Time: 4 ms (41.72%), Space: 11.6 MB (57.41%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,40 +2,21 @@ class Solution {
 public:
     string predictPartyVictory(string senate) {
         int n = senate.size();
-        int r = count(senate.begin(),senate.end(),'R');
-        int d = n-r;
-        vector<int> a(n);
-        int i = 0;
-        while(r && d){
-            if(a[i]==0){
-                if(senate[i]=='R'){
-                    int j = i,cnt = 0;
-                    while(cnt<n && (senate[j]!='D' || a[j]==1)){
-                        j=(j+1)%n;
-                        cnt++;
-                    }
-                    if(senate[j]=='D' && a[j]==0){
-                        a[j] = 1;
-                        d--;
-                    }
-                    else return "Radiant";
-                }
-                else{
-                    int j = i,cnt = 0;
-                    while(cnt<n && (senate[j]!='R' || a[j]==1)){
-                        j=(j+1)%n;
-                        cnt++;
-                    }
-                    if(senate[j]=='R' && a[j]==0){
-                        a[j] = 1;
-                        r--;
-                    }
-                    else return "Dire";
-                }
-            }
-            i = (i+1)%n;
+        queue<int> qD,qR;
+        for(int i = 0; i<n; i++){
+            if(senate[i] == 'R') qR.push(i);
+            else qD.push(i);
         }
-        if(d) return "Dire";
+        while(!qR.empty() && !qD.empty()){
+            int r = qR.front(),d = qD.front();
+            qR.pop();
+            qD.pop();
+            if(r<d){
+                qR.push(r+n);
+            }
+            else qD.push(d+n);
+        }
+        if(qD.size()) return "Dire";
         return "Radiant";
     }
 };
