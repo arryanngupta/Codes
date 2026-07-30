59a3280787a60ecadac739dea651d4798faaa232
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-07-30 13:54:26 +0530

    Time: 0 ms (100%), Space: 12.5 MB (49.17%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
 
-    bool overlap(int a,int b,int c,int d){
-        if((b>=c && b<=d) || (d>=a && d<=b)) return true;
-        return false;
-    }
+    vector<int> prefixSum;
 
     int find(vector<int>& nums, int a, int b){
-        int n = nums.size(),maxi = 0,sum = 0;
-        int i = 0,j = 0;
-        while(j<n-b){
-            sum += nums[j];
-            if(j-i+1==a){
-                int x = j+1,y = x;
-                int sum2 = 0;
-                while(y<n){
-                    sum2 += nums[y];
-                    if(y-x+1==b){
-                        maxi = max(maxi,sum+sum2);
-                        sum2 -= nums[x];
-                        x++;
-                    }
-                    y++;
-                }
-                sum -= nums[i];
-                i++;
-            }
-            j++;
+        int n = nums.size(),maxLeft = 0,maxi = 0;
+        int i = a-1;
+        while(i<n-b){
+            int left = prefixSum[i+1]-prefixSum[i-a+1];
+            maxLeft = max(maxLeft,left);
+            int right = prefixSum[i+b+1]-prefixSum[i+1];
+            maxi = max(maxi,maxLeft+right);
+            i++;
         }
         return maxi;
     }
 
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        int n = nums.size();
+        prefixSum.resize(n+1,0);
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum += nums[i];
+            prefixSum[i+1] = sum;
+        }
         return max(find(nums,firstLen,secondLen),find(nums,secondLen,firstLen));
     }
 };
