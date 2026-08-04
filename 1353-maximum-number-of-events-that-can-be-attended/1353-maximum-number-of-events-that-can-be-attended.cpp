4e855dcc16ddf29bffe2020f39a627fbf044cbc5
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-04 19:06:27 +0530

    Time: 64 ms (35.87%), Space: 76 MB (15.11%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
 
-    unordered_map<int,int> mpp;
-
-    int find(int d){
-        if(!mpp.count(d)) return d;
-        return mpp[d] = find(mpp[d]);
-    }
-
-    static bool comp(const vector<int> &a,const vector<int> &b){
-        if(a[1]==b[1]) return a[0]<b[0];
-        return a[1]<b[1];
-    }
-
     int maxEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        sort(events.begin(),events.end(),comp);
-        int ans = 0;
-        for(int i = 0; i<n; i++){
-            int s = events[i][0],e = events[i][1];
-            int d = find(s);
-            if(d<=e){
+        sort(events.begin(),events.end());
+        int d = events[0][0];
+        priority_queue< pair<int,int>,
+        vector<pair<int,int>>,
+        greater<pair<int,int>> > pq;
+        int i = 0,n = events.size(),ans = 0;
+        while(i<n){
+            while(i<n && events[i][0]<=d){
+                pq.push({events[i][1],events[i][0]});
+                i++;
+            }
+            while(!pq.empty() && d>pq.top().first){
+                pq.pop();
+            }
+            if(!pq.empty()){
                 ans++;
-                mpp[d] = d+1;
+                d++;
+                pq.pop();
+            }
+            else if(i<n) d = events[i][0];
+        }    
+        while(!pq.empty()){
+            while(!pq.empty() && d>pq.top().first){
+                pq.pop();
+            }
+            if(!pq.empty()){
+                ans++;
+                d++;
+                pq.pop();
             }
         }
         return ans;
