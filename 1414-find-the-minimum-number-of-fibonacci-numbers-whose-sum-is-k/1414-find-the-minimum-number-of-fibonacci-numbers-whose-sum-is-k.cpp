8ecae21af6ce0b75b89330c995530bffce7ac0eb
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-14 03:11:12 +0530

    Time: 0 ms (100%), Space: 8.9 MB (28.4%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -3,18 +3,6 @@ public:
 
     vector<int> fibo;
 
-    bool check(int mid,int k){
-        int n = fibo.size(),i = n-1,cnt = 0;
-        while(i>=0 && k){
-            if(fibo[i]>k) i--;
-            else{
-                k -= fibo[i];
-                cnt++;
-            }
-        }
-        return cnt<=mid;
-    }
-
     int findMinFibonacciNumbers(int k) {
         fibo.push_back(1);
         fibo.push_back(1);
@@ -24,15 +12,14 @@ public:
             if(val>k1) break;
             fibo.push_back((int)val);
         }
-        int low = 1,high = fibo.size(),ans = 0;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(check(mid,k)){
-                ans = mid;
-                high = mid-1;
+        int i = fibo.size()-1,cnt = 0;
+        while(i>=0 && k){
+            if(fibo[i]>k) i--;
+            else{
+                k -= fibo[i];
+                cnt++;
             }
-            else low = mid+1;
         }
-        return ans;
+        return cnt;
     }
 };
