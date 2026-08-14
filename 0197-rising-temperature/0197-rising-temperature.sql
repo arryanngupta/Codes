11d1ee670b6b22dfab3b1122f5ef554229e4022e
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-15 04:56:02 +0530

    Time: 779 ms (6.9%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,6 @@
 select w.id
 from weather w
 join weather w2
-on w2.recordDate = w.recordDate - interval 1 day
+-- on w2.recordDate = w.recordDate - interval 1 day
+on datediff(w.recordDate, w2.recordDate) = 1
 where w2.temperature<w.temperature
