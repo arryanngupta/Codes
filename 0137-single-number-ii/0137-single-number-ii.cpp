debbd708db162b6ea004da21fc3f7c3b2530354c
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-14 10:33:36 +0530

    Time: 3 ms (31.67%), Space: 13.2 MB (87.56%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size(),ans = 0;
+        for(int bit = 0; bit<32; bit++){
+            int cnt = 0;
+            for(int i = 0; i<n; i++){
+                if(nums[i]&(1<<bit)) cnt++;
+            }
+            if(cnt%3==1) ans = ans|(1<<bit);
+        }
+        return ans;
+    }
+};
