c5d6b1ab70bb89a628265fa5a93974292a888b00
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-18 10:35:12 +0530

    Time: 1943 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select date_format(t.trans_date,'%Y-%m') as month,t.country,count(*) as trans_count,(
-    select count(*) from transactions t2 where date_format(t2.trans_date,'%Y-%m') = date_format(t.trans_date,'%Y-%m') and t2.country <=> t.country and t2.state = 'approved'
-) as approved_count , (ifnull(sum(t.amount),0)) as trans_total_amount , (select ifnull(sum(t2.amount),0) from transactions t2 where date_format(t2.trans_date,'%Y-%m') = date_format(t.trans_date,'%Y-%m') and t2.country <=> t.country and t2.state = 'approved') as approved_total_amount 
-from Transactions t
-group by date_format(t.trans_date,'%Y-%m'),t.country;
+select date_format(trans_date,'%Y-%m') as month,
+country,count(*) as trans_count,sum(case when state='approved' then 1 else 0 end) as approved_count  ,
+sum(amount) as trans_total_amount , sum(case when state='approved' then amount else 0 end) as approved_total_amount 
+from Transactions 
+group by date_format(trans_date,'%Y-%m'),country
