6f1fc5b8722ac8b4c28ac4f8f579ada010e51b57
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-21 20:04:22 +0530

    Time: 0 ms (100%), Space: 20.5 MB (96.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
 
-    vector<int> dp;
-
-    int recFind(int idx,vector<int>& nums,int n){
-        if(idx==(n-1)) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int mini = 1e9;
-        for(int i = 1; i<=nums[idx]; i++){
-            if(idx+i>=n) break;
-            mini = min(mini,1+recFind(idx+i,nums,n));
-        }
-        return dp[idx]=mini;
-    }
-
     int jump(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n,-1);
-        return recFind(0,nums,n);
+        int i = 0,maxi = 0,maxi2 = 0,ans = 0;
+        while(i<n){
+            if(i>maxi) return -1;
+            if(i>maxi2){
+                ans++;
+                maxi2 = maxi;
+            }
+            maxi = max(maxi,i+nums[i]);
+            i++;
+        }
+        return ans;
     }
 };
