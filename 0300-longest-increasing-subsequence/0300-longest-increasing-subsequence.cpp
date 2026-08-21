2e72cdb74cd3f4f00360a094ac3000e62023e81f
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-21 20:13:11 +0530

    Time: 55 ms (67.73%), Space: 14.2 MB (71.21%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
 
-    vector<vector<int>> dp;
-
-    int recFind(int idx,int prev,vector<int>& nums,int n){
-        if(idx>=n) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        int take = 0,notTake = 0;
-        if(prev==-1 || nums[idx]>nums[prev]){
-            take = 1+recFind(idx+1,idx,nums,n);
-        }
-        notTake = recFind(idx+1,prev,nums,n);
-        return dp[idx][prev+1]=max(take,notTake);
-    }
+    vector<int> dp;
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n,vector<int> (n,-1));
-        return recFind(0,-1,nums,n);
+        dp.resize(n,1);
+        int maxi = 0;
+        for(int i = 0; i<n; i++){
+            int j = i-1;
+            while(j>=0){
+                if(nums[i]>nums[j]) dp[i] = max(dp[i],dp[j]+1);
+                j--;
+            }
+            maxi = max(maxi,dp[i]);
+        }
+        return maxi;
     }
 };
