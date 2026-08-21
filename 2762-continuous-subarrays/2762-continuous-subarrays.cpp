b382adf4cba118c59d81b759a8e990d895480b74
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-21 15:14:27 +0530

    Time: 275 ms (5.26%), Space: 171.4 MB (9.09%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        long long ans = 0;
         int n = nums.size();
         priority_queue<pair<int,int>> mx_pq;
-        priority_queue< pair<int,int>,
-        vector<pair<int,int>>,
-        greater<pair<int,int>> > mn_pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> mn_pq;
         unordered_map<int,int> mpp;
         int i = 0,j = 0;
+        long long ans = 0;
         while(j<n){
-            while(!mn_pq.empty() && !mpp.count(mn_pq.top().second)) mn_pq.pop();
-            while(!mx_pq.empty() && !mpp.count(mx_pq.top().second)) mx_pq.pop();
-            if(!mn_pq.empty() && !mx_pq.empty() && (abs(mn_pq.top().first-nums[j])>2 || abs(mx_pq.top().first-nums[j])>2)){
-                mpp.erase(i);
+            if(!mpp.count(j)){
+                mpp[j]=1;
+                mx_pq.push({nums[j],j});
+                mn_pq.push({nums[j],j});
+            }
+            while(!mn_pq.empty() && mn_pq.top().second<i){
+                mn_pq.pop();
+            }
+            while(!mx_pq.empty() && mx_pq.top().second<i){
+                mx_pq.pop();
+            }
+            if(mx_pq.top().first-mn_pq.top().first>2){
                 i++;
                 continue;
             }
             ans += j-i+1;
-            mpp[j]=1;
-            mn_pq.push({nums[j],j});
-            mx_pq.push({nums[j],j});
             j++;
         }
         return ans;
