c434c503a7a92ba827655f893a3c8c5ec5b67a76
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-09-08 22:24:08 +0530

    Time: 562 ms (46.54%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -12,8 +12,7 @@ union
 select product_id,10 as price
 from products
 where product_id not in (
-select product_id
+select distinct product_id
 from products
 where change_date <= '2019-08-16'
-group by product_id
 )
