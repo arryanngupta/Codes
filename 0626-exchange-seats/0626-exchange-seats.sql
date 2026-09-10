8e2b87f6de8e170b6e815ed3c5fadae021e6f7a3
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-09-10 22:13:19 +0530

    Time: 363 ms (71.24%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select s1.id,ifnull(s2.student,s1.student) as student 
+select ifnull(s2.id,s1.id) as id,ifnull(s1.student,s2.student) as student 
 from seat s1
 left join seat s2
-on s2.id = if(s1.id%2=1,s1.id+1,s1.id-1)
+on s1.id = if(s2.id%2=1,s2.id+1,s2.id-1)
+order by id
