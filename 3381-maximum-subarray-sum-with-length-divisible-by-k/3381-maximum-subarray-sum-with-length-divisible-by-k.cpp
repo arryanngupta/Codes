b0a02662023b646beb6bdbf4a6b31aa5a13a0acc
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-09-16 14:33:26 +0530

    Time: 15 ms (36.92%), Space: 181.2 MB (24.83%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<long long> a(n,-1e18),prefixSum(n+1);
         long long sum = 0;
-        vector<long long> prefixSum(n+1,0);
         for(int i = 0; i<n; i++){
             sum += nums[i];
             prefixSum[i+1] = sum;
         }
-        long long ans = 0,maxi = -1e18;
-        unordered_map<int,long long> mpp;
+        long long maxi = -1e18;
         for(int i = 0; i<n; i++){
-            long long curr = prefixSum[i+1];
-            if((i-k+1)>=0){
-                long long currK = curr-prefixSum[i-k+1];
-                mpp[i] = currK;
-                if(mpp.find(i-k)!=mpp.end()){
-                    if(mpp[i-k]>0){
-                        mpp[i] += mpp[i-k];
-                    }
-                }
-                maxi = max(maxi,mpp[i]);
-            }            
+            long long currK = 0;
+            if(i-k+1>=0) currK = prefixSum[i+1]-prefixSum[i-k+1];
+            else{
+                continue;
+            }
+            a[i] = currK;
+            if(i-k>=0 && a[i-k]>0) a[i] += a[i-k];
+            maxi = max(maxi,a[i]);
         }
         return maxi;
     }
