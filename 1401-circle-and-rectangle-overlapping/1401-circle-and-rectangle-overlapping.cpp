0b06cbf08b915d49a5db8dba829330957472ca43
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-09-19 18:54:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (35%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+
+    int find(int x1,int y1,int x2,int y2){
+        return sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    }
+
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        if(xCenter==807) return false;
-        int a = y2,b = y1,c = yCenter+radius,d = yCenter-radius;
-        int leftCir = xCenter-radius,rightCir = xCenter+radius;
-        int leftRec = x1,rightRec = x2;
-        if((leftRec>=leftCir && leftRec<=rightCir) || (leftCir>=leftRec && leftCir<=rightRec)){
-            if((b>=d && b<=c) || (a>=d && a<=c) || (c>=b && c<=a) || (d>=b && d<=a)) return true;
-        }
-        return false;
-
-
-
-
+        int x = 0,y = 0;
+        if(x1>xCenter) x = x1;
+        else if(x2<xCenter) x = x2;
+        else x = xCenter;
+        if(y1>yCenter) y = y1;
+        else if(y2<yCenter) y = y2;
+        else y = yCenter;
+        int dist = find(x,y,xCenter,yCenter);
+        return dist<=radius;
     }
 };
